ec2428d722fee3efdb003e9573acfebd542434eb
danielsergio22 <ds.daniel.sergio@gmail.com>
2022-02-12 09:36:02 -0300

    desafioGFT

--- desafio3/pessoa.java
+++ desafio3/pessoa.java
@@ -8,6 +8,22 @@ public class pessoa {
         this.setNome(nome);
         this.setCargo(cargo);
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
     
     
 }
